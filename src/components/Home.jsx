f129b7510f18185fc67061cd9901f6db97bced2e
Abraham Castillo <32278636+castilloabraham@users.noreply.github.com>
2024-03-10 13:05:44 -0400

    maqueta principal Navbar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,9 +5,12 @@ import appFirebase from "../credentials";
 import { getAuth, signOut } from "firebase/auth";
 const auth = getAuth(appFirebase)
 
+import Navbar from './Navbar'
+
 const Home = ({correoUsuario}) => {
     return (
         <div>
+            <Navbar/>
             <h2>Bienvenido: {correoUsuario} <button className="btn-primary" onClick={()=> signOut(auth)}>Logout</button></h2>
         </div>
     )
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import appFirebase from "../credentials";
+import { getAuth, signOut } from "firebase/auth";
+const auth = getAuth(appFirebase)
+
+
+
+const Navbar = () => {
+    return (
+
+        <nav className="navbar navbar-expand-lg">
+            <div className="container-fluid">
+                <a className="navbar-brand" href="#">LOGO</a>
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarScroll" aria-controls="navbarScroll" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarScroll">
+                    
+                    <div className="nav-item">
+                        <a className="nav-link active" aria-current="page" href="#">Home</a>
+                    </div>
+                    <div className="mx-auto d-flex justify-content-center ">
+                        <form className="nav-item d-flex">
+                            <input className="form-control me-2" type="search" placeholder="Buscar Juego" aria-label="Search"/>
+                            <button className="btn btn-outline-success" type="submit">Buscar</button>
+                        </form>
+                    </div>
+                    <div className="nav-item dropdown ms-auto">
+                        <a className="nav-link dropdown-toggle" href="#" id="navbarScrollingDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Nombre usuario
+                        </a>
+                        <ul className="dropdown-menu" aria-labelledby="navbarScrollingDropdown">
+                            <li><a className="dropdown-item" href="#">Mi perfil</a></li>
+                            <li><hr className="dropdown-divider"/></li>
+                            <li><a className="dropdown-item" href="#" onClick={()=> signOut(auth)} >Cerrar Sesión</a></li>
+                        </ul>
+                    </div>
+                
+                </div>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar
