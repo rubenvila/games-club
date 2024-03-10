72e234809ef8de3dea59ee0099a8a2023885253c
Abraham Castillo <32278636+castilloabraham@users.noreply.github.com>
2024-03-10 16:56:02 -0400

    Llamado de informacion de la db e integracion con el componente cardClub

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,23 +3,52 @@
 import React from "react";
 import appFirebase from "../credentials";
 import { getAuth, signOut } from "firebase/auth";
+
+import { getFirestore, collection, getDocs } from "firebase/firestore"; 
+import "firebase/firestore"; 
+import { useState, useEffect } from "react";
+import { Link } from "react-router-dom"; 
+
+
 const auth = getAuth(appFirebase)
+ 
 
 import Navbar from './Navbar'
 import CardClub from './CardClub'
 
 const Home = ({correoUsuario}) => {
+    const db = getFirestore(appFirebase); 
+ 
+    const [clublist, setClublist] = useState([]); 
+ 
+    const cargarClubes = async () => { 
+        let list = [] 
+   
+        const response = collection(db,"Clubes"); 
+        const date = await getDocs(response) 
+        const prueba = date.docs.map((doc) => list.push(doc.data())) 
+        setClublist(list) 
+    } 
+ 
+    useEffect(() => { 
+        cargarClubes() 
+    }, []) 
+
+
+
     return (
         <div>
             <Navbar/>
             <h2>Bienvenido: {correoUsuario}</h2>
             <div className="row">
-
-                <CardClub/>
+                {clublist.map(objeto =>
+                    <CardClub info = {objeto}/>
+                )}
             </div>
 
         </div>
     )
 }
+
 
 export default Home
--- src/credentials.js
+++ src/credentials.js
@@ -1,18 +1,21 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyBvY6Feq4PBJqyfEQmip5veAQ77Rai2Ifg",
-  authDomain: "games-club-project.firebaseapp.com",
-  projectId: "games-club-project",
-  storageBucket: "games-club-project.appspot.com",
-  messagingSenderId: "330314818590",
-  appId: "1:330314818590:web:171572d322dc186102ba0f"
-};
-
-// Initialize Firebase
-const appFirebase = initializeApp(firebaseConfig);
+// Import the functions you need from the SDKs you need 
+import  { initializeApp } from "firebase/app"; 
+import { getFirestore } from "@firebase/firestore"; 
+// TODO: Add SDKs for Firebase products that you want to use 
+// https://firebase.google.com/docs/web/setup#available-libraries 
+ 
+// Your web app's Firebase configuration 
+const firebaseConfig = { 
+  apiKey: "AIzaSyBvY6Feq4PBJqyfEQmip5veAQ77Rai2Ifg", 
+  authDomain: "games-club-project.firebaseapp.com", 
+  projectId: "games-club-project", 
+  storageBucket: "games-club-project.appspot.com", 
+  messagingSenderId: "330314818590", 
+  appId: "1:330314818590:web:171572d322dc186102ba0f" 
+}; 
+ 
+// Initialize Firebase 
+const appFirebase = initializeApp(firebaseConfig); 
+ 
+//const bd = firebase.firestore() 
 export default appFirebase;
