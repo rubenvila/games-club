2a0a63c23d29bce1f548fb03cede3c0ba767f035
rubenvila <ruben.vila@unimet.edu.ve>
2024-03-10 02:09:08 -0400

    BDD

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,11 @@
 /* eslint-disable no-unused-vars */
+import swal from 'sweetalert';
 import React, { useState } from "react";
 import appFirebase from '../credentials'
 import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth'
+import { getRedirectResult, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
+
+const provider = new GoogleAuthProvider()
 
 const auth = getAuth(appFirebase)
 
@@ -18,18 +22,41 @@ const Login = () => {
             try {
             await createUserWithEmailAndPassword(auth, correo, contraseña)
         } catch (error) {
-            alert("Asegúrese que la contraseña tenga más de 8 caracteres")
+            swal("Asegúrese que la contraseña tenga más de 8 caracteres")
         }
         } else {
             try {
             await signInWithEmailAndPassword(auth, correo, contraseña)
         } catch(error) {
-            alert("Correo o contraseña erróneas")
+            swal("Correo o contraseña erróneas")
         }
 
 
         }
 
+    }
+
+    function call_login_google() {
+        signInWithPopup(auth, provider)
+  .then((result) => {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    const credential = GoogleAuthProvider.credentialFromResult(result);
+    const token = credential.accessToken;
+    // The signed-in user info.
+    const user = result.user;
+    // IdP data available using getAdditionalUserInfo(result)
+    // ...
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GoogleAuthProvider.credentialFromError(error);
+    // ...
+  });
+        
     }
 
     return (
@@ -45,7 +72,8 @@ const Login = () => {
                                 <button className="btnform">{registrando ? "Registrarse" : "Inicia sesión"}</button>
                             </form>
                             <h4 className="texto">{registrando ? "Si ya tienes cuenta " : "¿No tienes cuenta? "}<button className="btnswitch" onClick={()=> setRegistrando(!registrando)}>{registrando ? "Inicia sesión" : "Regístrate"}</button></h4>
-                        </div>
+                            <h4 className="texto" onClick={call_login_google}>Inicia sesión con <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2f/Google_2015_logo.svg/1200px-Google_2015_logo.svg.png" alt="Iniciar sesión con Google" className="img_google" onClick={call_login_google}/>
+                            </h4></div>
                     </div>
                     
                 </div>
