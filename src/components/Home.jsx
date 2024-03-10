913db3a67704082804b254e4151292ce50178eab
rubenvila <ruben.vila@unimet.edu.ve>
2024-03-09 23:08:19 -0400

    Registro y login

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,14 @@
+/* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 import React from "react";
+import appFirebase from "../credentials";
+import { getAuth, signOut } from "firebase/auth";
+const auth = getAuth(appFirebase)
 
-const Home = () => {
+const Home = ({correoUsuario}) => {
     return (
         <div>
-            <h2>BIENVENIDO</h2>
+            <h2>Bienvenido: {correoUsuario} <button className="btn-primary" onClick={()=> signOut(auth)}>Logout</button></h2>
         </div>
     )
 }
